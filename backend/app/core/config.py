b9a8ebd6a858afe24584cf6faa87f64e0177d50c
InBloom151 <inbloom151@yandex.ru>
2025-04-14 21:40:57 +0300

    [dev][feat] security module for JWT added

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,6 +4,11 @@ import os
 class Settings(BaseSettings):
     DATABASE_URL: str
 
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+
     class Config:
         env_file = os.getenv("ENV_FILE", ".env")
         extra = "allow"
