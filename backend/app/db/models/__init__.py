9527123e16b6e218f78efcdc65a1f7421e7a0b9b
InBloom151 <inbloom151@yandex.ru>
2025-04-22 16:24:58 +0300

    [dev][fix] models imports fix

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,9 +1,11 @@
 from .category import Category
 from .landlord_detail import LandlordDetail
+from .transport import Transport
 from .user import User
 
 __all__ = [
     "User",
     "LandlordDetail",
     "Category",
+    "Transport",
 ]
